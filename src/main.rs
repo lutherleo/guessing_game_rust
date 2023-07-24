b20b513d88bd2566a7d94a72b3874a380d63d268
Zangetsu <d4c@kali.www.soulsociety.com>
2023-07-24 05:48:50 +0530

    file upload

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use std::io;
+use rand::Rng;
+use std::cmp::Ordering; 
+
+fn main() {
+    println!("Guess the number:");
+    
+    let secret_number = rand::thread_rng().gen_range(0..=100);
+
+    loop {
+        
+        let mut guess = String::new();
+
+        io::stdin()
+            .read_line(&mut guess) //this is a comment
+            .expect("Failed to read line");
+        let guess:u32 = guess.trim().parse().expect("Please type a number:");
+
+        println!("You guessed {guess}");
+    
+        match guess.cmp(&secret_number){
+            Ordering::Less => println!("Too small"),
+            Ordering::Greater => println!("Too large"),
+            Ordering::Equal => {
+		println!("You win");
+		break;
+		}
+    	}
+    
+	}
+}
